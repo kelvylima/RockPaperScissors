11ee632e262c577b20ec7fb5eebb05698f714b83
Kelvy Lima <kelvy2@gmail.com>
2022-08-11 15:57:54 -0300

    implement score, images and a couple of functions

--- script.js
+++ script.js
@@ -5,18 +5,20 @@ function RandomNumber() {
 
 
 function Getcomputerchoice() {
-
     let computerchoice = RandomNumber()
     let computermove = ''
     switch (computerchoice) {
         case 0:
             computermove = 'Rock';
+            Createimage('rock', '#computerchoice')
             break;
         case 1:
             computermove = 'Paper';
+            Createimage('paper', '#computerchoice')
             break;
         case 2:
             computermove = 'Scissors';
+            Createimage('scissors', '#computerchoice')
             break;            
     }
     
@@ -90,9 +92,8 @@ function Playround(computerselection, playerselection) {
 
 }
 
+
 function Game() {
-    let playerscore = document.getElementById('playerscore')
-    let computerscore = document.getElementById('computerscore')
     let playerpoints = 0;
     let computerpoints = 0;
     //results
@@ -100,39 +101,94 @@ function Game() {
     //rock button
     const rock = document.querySelector('#rock');
     rock.addEventListener('click', function() {
-        
+        Createimage('rock', '#playerchoice')
         const battle = Playround(Getcomputerchoice(), 'Rock')
         results.textContent = battle[0]
         if (battle[1] === 1) {
-            playerscore.textContent = playerpoints+=1
+            playerpoints+=1
+            Onoffneon(playerpoints, "#playerscore")
         } else if (battle[1] === 0) {
-            computerscore.textContent = computerpoints+=1
+            computerpoints+=1
+            Onoffneon(computerpoints, "#computerscore")
         }
+        Gameover(playerpoints, computerpoints, results)
     });
     //paper button
     const paper = document.querySelector('#paper');
     paper.addEventListener('click', function() {
+        Createimage('paper', '#playerchoice')
         const battle = Playround(Getcomputerchoice(), 'Paper')
         results.textContent = battle[0]
         if (battle[1] === 1) {
-            playerscore.textContent = playerpoints+=1
+            playerpoints+=1
+            Onoffneon(playerpoints, "#playerscore")
         } else if (battle[1] === 0) {
-            computerscore.textContent = computerpoints+=1
+            computerpoints+=1
+            Onoffneon(computerpoints, "#computerscore")
         }
+        Gameover(playerpoints, computerpoints, results)
     });
     //scissor button
     const scissor = document.querySelector('#scissor');
     scissor.addEventListener('click', function() {
+        Createimage('scissors', '#playerchoice')
         const battle = Playround(Getcomputerchoice(), 'Scissors')
         results.textContent = battle[0]
         if (battle[1] === 1) {
-            playerscore.textContent = playerpoints+=1
+            playerpoints+=1
+            Onoffneon(playerpoints, "#playerscore")
         } else if (battle[1] === 0) {
-            computerscore.textContent = computerpoints+=1
+            computerpoints+=1
+            Onoffneon(computerpoints, "#computerscore")
         }
+        Gameover(playerpoints, computerpoints, results)
     });
     
     
 }
+
+function Gameover(playerscore, computerscore, results) {
+    let gameover = ''
+    if(playerscore === 5) {
+        results.textContent = 'YOU WIN! CONGRATULATIONS!'
+        gameover = true
+    } else if(computerscore === 5) {
+        results.textContent = 'YOU LOSE! GAME OVER'
+        gameover = true
+    }
+    if(gameover){
+        let yesbutton = createElement('button')
+    }
+}
+
+function Onoffneon(count, idstring){
+    var ogtext = document.querySelector(`${idstring}`).textContent
+    for(let i of ogtext){
+        var numbertext = Number(i)
+        if(numbertext === count){
+            var newtext = ogtext.replace(`${i}`, `<span class="neonon text">${i}</span>`)
+        }
+                  
+    }
+        
+    document.querySelector(`${idstring}`).innerHTML = newtext
+}
+
+function Createimage(choice, idstring){
+    let div = document.querySelector(`${idstring}`)
+    let imgid = ''
+    if(idstring === "#computerchoice"){imgid = "computer"} else{imgid = "player"}
+    let checkimage = document.querySelector(`#${imgid}`);
+    if(checkimage !== null){
+        checkimage.remove()
+    }
+    let img = document.createElement('img')
+        img.classList.add('image')
+        img.setAttribute('id', `${imgid}`)
+        img.src = `imgs/${choice}.png`
+        div.appendChild(img);
+
+}
+
 
 Game()
